7a3024c803a153249a9628d1000b46e43bbf8216
Ying Zhong <0x00eeee@gmail.com>
2025-07-17 20:29:30 +0800

    Integrate latest markedit-theming

--- src/themes.ts
+++ src/themes.ts
@@ -1,16 +1,10 @@
 import { Extension } from '@codemirror/state';
 import { catppuccinLatte, catppuccinFrappe, catppuccinMacchiato, catppuccinMocha } from '@catppuccin/codemirror';
-import { flavors } from '@catppuccin/palette';
-import type { FlavorName, CatppuccinColors } from '@catppuccin/palette';
+import type { FlavorName } from '@catppuccin/palette';
 import { CustomTheme } from 'markedit-theming';
 
 export function themeNamed(name: FlavorName): CustomTheme {
-  return {
-    extension: extensions[name],
-    colors: {
-      visibleSpace: colors[name].overlay1.hex,
-    },
-  };
+  return { extension: extensions[name] };
 }
 
 export { FlavorName };
@@ -20,11 +14,4 @@ const extensions: Record<FlavorName, Extension> = {
   frappe: catppuccinFrappe,
   macchiato: catppuccinMacchiato,
   mocha: catppuccinMocha,
-};
-
-const colors: Record<FlavorName, CatppuccinColors> = {
-  latte: flavors.latte.colors,
-  frappe: flavors.frappe.colors,
-  macchiato: flavors.macchiato.colors,
-  mocha: flavors.mocha.colors,
 };
