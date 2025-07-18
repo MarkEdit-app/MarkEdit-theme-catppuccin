5d6d419e86b7282f7958e812bc5c611dfed24d48
Ying Zhong <0x00eeee@gmail.com>
2025-07-18 20:57:15 +0800

    Simplify enabledMode with new markedit-theming

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 import { overrideThemes } from 'markedit-theming';
-import { enabledMode, lightTheme, darkTheme } from './src/settings';
+import { lightTheme, darkTheme, settingsKey } from './src/settings';
 import { themeNamed } from './src/themes';
 
 overrideThemes({
-  light: ['both', 'light'].includes(enabledMode) ? themeNamed(lightTheme) : undefined,
-  dark: ['both', 'dark'].includes(enabledMode) ? themeNamed(darkTheme) : undefined,
+  light: themeNamed(lightTheme),
+  dark: themeNamed(darkTheme),
+  options: { settingsKey },
 });
--- src/settings.ts
+++ src/settings.ts
@@ -2,12 +2,18 @@ import { MarkEdit } from 'markedit-api';
 import type { JSONObject, JSONValue } from 'markedit-api';
 import { FlavorName } from './themes';
 
+const settingsKey = 'extension.markeditThemeCatppuccin';
 const userSettings = toObject(MarkEdit.userSettings);
-const rootValue = toObject(userSettings['extension.markeditThemeCatppuccin']);
+const rootValue = toObject(userSettings[settingsKey]);
 
-export const enabledMode = (rootValue.enabledMode ?? 'both') as string;
-export const lightTheme = (rootValue.lightTheme ?? 'latte') as FlavorName;
-export const darkTheme = (rootValue.darkTheme ?? 'mocha') as FlavorName;
+const lightTheme = (rootValue.lightTheme ?? 'latte') as FlavorName;
+const darkTheme = (rootValue.darkTheme ?? 'mocha') as FlavorName;
+
+export {
+  lightTheme,
+  darkTheme,
+  settingsKey,
+};
 
 function toObject(jsonValue: JSONValue, defaultValue = {}) {
   return (jsonValue ?? defaultValue) as JSONObject;
